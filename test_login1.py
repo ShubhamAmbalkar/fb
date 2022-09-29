05fb7bfb94e07b34e7fb819329796958a0245639
ShubhamAmbalkar <102300810+ShubhamAmbalkar@users.noreply.github.com>
2022-09-29 22:28:17 +0530

    Add files via upload

---
+++ test_login1.py
@@ -0,0 +1,16 @@
+def test_1():
+
+    A = 56
+    b = 45
+    assert A+b >=65
+
+def test_2():
+
+    b = 'shubham'
+    c = 'shubham ambalkar'
+    assert b in c
+def test_3 ():
+
+    h=90
+    c=99
+    assert h<=c
